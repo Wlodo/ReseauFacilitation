de52dd525acbb798e2fb45076900390defc47b6c
Wlodo <77634878+Wlodo@users.noreply.github.com>
2021-01-25 21:23:39 +0000

    Deploying to gh-pages from master @ 27215ee32007540a66eb78fd8316543b1150d2b6 🚀

---
+++ furniture.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="furniture" tilewidth="48" tileheight="48" tilecount="256" columns="16">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <image source="furniture.png" width="768" height="768"/>
+</tileset>
